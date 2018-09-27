fc3302a0797643a23bf1d0a5c2a93b647c557982
Alexandr Komar <909komar@gmail.com>
2018-09-27 14:15:58 +0300

    fix labelText

--- src/components/Select.js
+++ src/components/Select.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Select = (props) => {
-  const {id, name, value, options, onChange, error} = props;
+  const {id, name, value, options, onChange, labelText, error} = props;
 
   console.log(options);
 
@@ -16,7 +16,7 @@ const Select = (props) => {
 
   return (
     <div className="form-group">
-      <label htmlFor={id}>Country</label>
+      <label htmlFor={id}>{labelText}</label>
       <select
         className={"form-control " + (error ? 'is-invalid': '')}
         id={id}
